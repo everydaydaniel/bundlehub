c7c0760ac26155d4eda5128a733c4c827495e251
tyler stendara <tyler.stendara@ibm.com>
2020-05-21 18:37:56 -0500

    changing var names

--- api/storage.py
+++ api/storage.py
@@ -44,8 +44,8 @@ def search(industry):
 	client, db, collection = mongo_connection()
 	results = []
 	for stix_obj in collection.find():
-		hasKey = 'industry' in x.keys()
-		if(hasKey and x['industry'] == industry):
+		hasKey = 'industry' in stix_obj.keys()
+		if(hasKey and stix_obj['industry'] == industry):
 			del stix_obj['_id']
 			del stix_obj['industry']
 
