383829df057f45cb987e28c56e85a0fe23185f40
tyler stendara <tyler.stendara@ibm.com>
2020-05-27 14:01:00 -0500

    adding random industry label

--- api/generator.py
+++ api/generator.py
@@ -14,37 +14,38 @@ import datetime
 ##		Downloads CSV dataset and converts it to a STIX2 bundle		##
 def gen_from_url(url):
 
-	df = pd.read_csv(url)
-	stamp = datetime.datetime.now()
-	stix_objects = []
-	stix_observable_objects = []
-	stix_object_dict = {
+    df = pd.read_csv(url)
+    stamp = datetime.datetime.now()
+    stix_objects = []
+    stix_observable_objects = []
+    stix_object_dict = {
             "type": "observed-data",
             "objects":{},
             "first_observed": stamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
             "last_observed": stamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
             "number_observed": 1 }
 
-
+    
 	##		Create ObservedData		##
-	for sdo_index in range(len(df.index)):
-		stix_objects.append(IPv4Address(value=df.at[sdo_index, "ip"]))
-		stix_objects.append(UserAccount(user_id=df.at[sdo_index, "user"]))
-		stix_objects.append(File(name=df.at[sdo_index, "file"], hashes={"SHA-256":df.at[sdo_index, "hashes"]}))
-		stix_objects.append(DomainName(value=df.at[sdo_index, "url"]))
-		print("[STIX2 GEN] BUILDING SDO ", sdo_index)
+    for sdo_index in range(len(df.index)):
+        stix_objects.append(IPv4Address(value=df.at[sdo_index, "ip"]))
+        stix_objects.append(UserAccount(user_id=df.at[sdo_index, "user"]))
+        stix_objects.append(File(name=df.at[sdo_index, "file"], hashes={"SHA-256":df.at[sdo_index, "hashes"]}))
+        stix_objects.append(DomainName(value=df.at[sdo_index, "url"]))
+        
+        print("[STIX2 GEN] BUILDING SDO ", sdo_index)
 
 
-		for idx, stix_object in enumerate(stix_objects):
-			stix_object_dict["objects"][str(idx)] = json.loads(str(stix_object))
-		stix_observable_objects.append(parse(json.dumps(stix_object_dict)))
+        for idx, stix_object in enumerate(stix_objects):
+            stix_object_dict["objects"][str(idx)] = json.loads(str(stix_object))
+        stix_observable_objects.append(parse(json.dumps(stix_object_dict)))
 
+    industry = df.at[sdo_index, "industry"]
+    ##		Generate QRadar Event Bundle		##
+    stix_observable_objects.append(Identity(name="QRadar", identity_class="events"))
+    bundle = Bundle(stix_observable_objects)
 
-	##		Generate QRadar Event Bundle		##
-	stix_observable_objects.append(Identity(name="QRadar", identity_class="events"))
-	bundle = Bundle(stix_observable_objects)
-
-	return bundle
+    return {"bundle": bundle, "industry": industry}
 
 
 ##      Generates random bundle of SDO's        ##
@@ -52,6 +53,7 @@ def gen_random_bundle():
     
     md5 = randomsdo.hashing()
     new_observables = randomsdo.creatingObservedObject()
+    industry = randomsdo.randomIndustry()
 
     indicator = Indicator(
     name="File hash for malware variant",
@@ -76,4 +78,4 @@ def gen_random_bundle():
     target_ref=indicator.id)
 
     bundle = Bundle(indicator, observed, malware)
-    return bundle
+    return {"bundle": bundle, "industry": industry}
