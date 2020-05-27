99888e7dc229f58cf0196cfc5606dd41bca2ca7b
Andrew Campagna <Andrew.Campagna@ibm.com>
2020-05-27 16:01:46 -0500

    fixed issue with multi row csv

--- api/generator.py
+++ api/generator.py
@@ -12,12 +12,12 @@ import datetime
 
 
 ##		Downloads CSV dataset and converts it to a STIX2 bundle		##
-def gen_from_url(url):
+def gen_from_csv(url):
 
     df = pd.read_csv(url)
     stamp = datetime.datetime.now()
-    stix_objects = []
     stix_observable_objects = []
+    stix_objects = []
     stix_object_dict = {
             "type": "observed-data",
             "objects":{},
@@ -33,19 +33,20 @@ def gen_from_url(url):
         stix_objects.append(File(name=df.at[sdo_index, "file"], hashes={"SHA-256":df.at[sdo_index, "hashes"]}))
         stix_objects.append(DomainName(value=df.at[sdo_index, "url"]))
         
-        print("[STIX2 GEN] BUILDING SDO ", sdo_index)
+        print(f"[STIX2 GEN] BUILDING SDO {sdo_index}")
 
 
         for idx, stix_object in enumerate(stix_objects):
             stix_object_dict["objects"][str(idx)] = json.loads(str(stix_object))
         stix_observable_objects.append(parse(json.dumps(stix_object_dict)))
+        stix_objects.clear()
 
-    industry = df.at[sdo_index, "industry"]
+
     ##		Generate QRadar Event Bundle		##
     stix_observable_objects.append(Identity(name="QRadar", identity_class="events"))
     bundle = Bundle(stix_observable_objects)
 
-    return {"bundle": bundle, "industry": industry}
+    return bundle
 
 
 ##      Generates random bundle of SDO's        ##
