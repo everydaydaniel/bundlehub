9e727c5f0d900b8e9c44e63a296f1fd33b20c68a
tyler stendara <tyler.stendara@ibm.com>
2020-06-01 14:57:20 -0500

    get request for all industries

---
+++ api/stix_generators/get_Industries.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def all_Industries():
+    r = requests.get('https://api.xforce.ibmcloud.com/industries', headers={ 'Accept': 'application/json',
+    'Authorization': 'Basic YzY4YzAzYTEtOTIzZi00OTg4LTgyYTYtNjFiYzFiNmY1Zjc2OmVkYTY3MTZkLTgzMTktNDFjNS1hOTY2LTZhMTgzMzNhMzIzOA=='
+    })
+    data = json.loads(r.content)
+    rows_of_data = data["rows"]
+    allIndustries = []
+
+    for objs in rows_of_data:
+        curInudstry = objs['name'].replace('Industry Profile', '').strip()
+        allIndustries.append(curInudstry)
+
+    return allIndustries
